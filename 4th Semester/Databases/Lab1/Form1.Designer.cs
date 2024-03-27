0f63a7efcba32c7afa32b574951160108fb9b9f2
engelInes <153627796+engelInes@users.noreply.github.com>
2024-03-27 21:34:08 +0200

    Add files via upload

---
+++ 4th Semester/Databases/Lab1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace l1WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.employerLabel = new System.Windows.Forms.Label();
+            this.jobLabel = new System.Windows.Forms.Label();
+            this.EmployerView = new System.Windows.Forms.DataGridView();
+            this.JobView = new System.Windows.Forms.DataGridView();
+            this.updateButton = new System.Windows.Forms.Button();
+            /*this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();*/
+            ((System.ComponentModel.ISupportInitialize)(this.EmployerView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JobView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // employerLabel
+            // 
+            this.employerLabel.AutoSize = true;
+            this.employerLabel.Location = new System.Drawing.Point(527, 329);
+            this.employerLabel.Name = "employerLabel";
+            this.employerLabel.Size = new System.Drawing.Size(94, 25);
+            this.employerLabel.TabIndex = 3;
+            this.employerLabel.Text = "Employer";
+            //.employerLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // jobLabel
+            // 
+            this.jobLabel.AutoSize = true;
+            this.jobLabel.Location = new System.Drawing.Point(1035, 329);
+            this.jobLabel.Name = "jobLabel";
+            this.jobLabel.Size = new System.Drawing.Size(45, 25);
+            this.jobLabel.TabIndex = 4;
+            this.jobLabel.Text = "Job";
+            //this.jobLabel.Click += new System.EventHandler(this.Table2_Click);
+            // 
+            // EmployerView
+            // 
+            /*this.EmployerView.AllowUserToAddRows = false;
+            this.EmployerView.AllowUserToDeleteRows = false;*/
+            this.EmployerView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployerView.Location = new System.Drawing.Point(833, 12);
+            this.EmployerView.Name = "EmployerView";
+            this.EmployerView.RowHeadersWidth = 72;
+            this.EmployerView.RowTemplate.Height = 31;
+            this.EmployerView.Size = new System.Drawing.Size(431, 368);
+            this.EmployerView.TabIndex = 0;
+            //.EmployerView.SelectionChanged += new System.EventHandler(this.EmployerView_SelectionChanged);
+            // 
+            // JobView
+            // 
+            /*this.JobView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.JobView.Location = new System.Drawing.Point(591, 93);
+            this.JobView.Name = "JobView";
+            this.JobView.RowHeadersWidth = 72;
+            this.JobView.Size = new System.Drawing.Size(431, 368);
+            this.JobView.TabIndex = 3;
+            this.JobView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.JobView_CellContentClick);
+            this.JobView.SelectionChanged += new System.EventHandler(this.JobView_SelectionChanged);*/
+            this.EmployerView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployerView.Location = new System.Drawing.Point(302, 12);
+            this.EmployerView.Name = "JobView";
+            this.EmployerView.RowHeadersWidth = 72;
+            this.EmployerView.RowTemplate.Height = 31;
+            this.EmployerView.Size = new System.Drawing.Size(478, 368);
+            this.EmployerView.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            /*this.addButton.Location = new System.Drawing.Point(93, 493);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(183, 45);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);*/
+            // 
+            // deleteButton
+            // 
+            /*this.deleteButton.Location = new System.Drawing.Point(778, 493);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(183, 45);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);*/
+            // 
+            // updateButton
+            // 
+            this.updateButton.AccessibleDescription = "updateButton";
+            this.updateButton.AccessibleName = "updateButton";
+            this.updateButton.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.updateButton.Location = new System.Drawing.Point(440, 493);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(183, 45);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1717, 716);
+            this.Controls.Add(this.updateButton);
+            /*this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);*/
+            this.Controls.Add(this.JobView);
+            this.Controls.Add(this.EmployerView);
+            this.Controls.Add(this.jobLabel);
+            this.Controls.Add(this.employerLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmployerView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.JobView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label employerLabel;
+        private System.Windows.Forms.Label jobLabel;
+        private System.Windows.Forms.DataGridView EmployerView;
+        private System.Windows.Forms.DataGridView JobView;
+        /*private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;*/
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ 4th Semester/Databases/Lab1/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace l1WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter daEmployer;
+        SqlDataAdapter daJob;
+        DataSet dset;
+        BindingSource bsEmployer;
+        BindingSource bsJob;
+        DataRelation EmployerJob;
+
+        SqlCommandBuilder cmdBuilder;
+
+        string queryEmployer;
+        string queryJob;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        /*void ReloadJobTableView()
+        {
+            if (dset.Tables["Job"] != null)
+            {
+                dset.Tables["Job"].Clear();
+            }
+            daJob.Fill(dset, "Job");
+            JobView.DataSource = bsJob;
+        }*/
+
+        void FillData()
+        {
+            connection = new SqlConnection(getConnectionString());
+            queryEmployer = "SELECT * FROM Employer";
+            queryJob = "SELECT * FROM Job";
+
+            //creating data adapters
+            daEmployer = new SqlDataAdapter(queryEmployer, connection);
+            daJob = new SqlDataAdapter(queryJob, connection);
+            dset = new DataSet();
+
+            daEmployer.Fill(dset, "Employer");
+            daJob.Fill(dset, "Job");
+
+            cmdBuilder = new SqlCommandBuilder(daJob);
+            //adding parent-child relationship
+            dset.Relations.Add("EmployerJob", dset.Tables["Employer"].Columns["companyID"],
+                dset.Tables["Job"].Columns["companyID"]);
+
+            //filling data into grid view using data binding
+            this.EmployerView.DataSource = dset.Tables["Employer"];
+            this.JobView.DataSource = this.EmployerView.DataSource;
+            this.JobView.DataMember = "EmployerJob";
+            /*bsEmployer = new BindingSource();
+            bsEmployer.DataSource = dset.Tables["Employer"];
+            bsJob = new BindingSource(bsEmployer, "EmployerJob");
+
+            this.EmployerView.DataSource = bsEmployer;
+            this.JobView.DataSource = bsJob;*/
+
+            cmdBuilder.GetUpdateCommand();
+        }
+
+        //implemented by me
+        string getConnectionString()
+        {
+            return "Data Source=DESKTOP-56RUGQC\\SQLEXPRESS;Initial Catalog=RecruitingPlatform;"+ "Integrated Security=true;";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                daEmployer.Update(dset, "Employer");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Database update did not work\n" + exception.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(exception.ToString());
+            }
+
+            /*connection = new SqlConnection("Data Source=DESKTOP-56RUGQC\\SQLEXPRESS;Initial Catalog=RecruitingPlatform;" + "Integrated Security=true;");
+            connection.Open();
+            daEmployer = new SqlDataAdapter("SELECT * FROM Employer", connection);
+            dset = new DataSet();
+            daEmployer.Fill(dset, "Employer");
+            EmployerView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            daJob = new SqlDataAdapter("SELECT * FROM Job", connection);
+            dset = new DataSet();
+            daJob.Fill(dset, "Job");
+            JobView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            DataColumn companyIDJob = dset.Tables["Job"].Columns["companyID"];
+            DataColumn companyIDEmployer = dset.Tables["Employer"].Columns["companyID"];
+            //EmployerJob = new DataRelation();
+            dset.Relations.Add("EmployerJob",
+                dset.Tables["Employer"].Columns["companyID"],
+                dset.Tables["Job"].Columns["companyID"]);
+
+            bsEmployer = new BindingSource();
+            bsEmployer.DataSource = dset;
+            bsEmployer.DataMember = "Employer";
+
+            bsJob = new BindingSource();
+            bsJob.DataSource = bsEmployer;
+            bsJob.DataMember = "EmployerJob";
+            EmployerView.DataSource = bsEmployer;*/
+        }
+
+        /*private void label1_Click(object sender, EventArgs e)
+        {
+            daJob.Update(dset, "Job");
+        }
+
+        private void Table2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            daJob.Update(dset, "Job");
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand(
+            "INSERT INTO Job(jobID, job_title, job_description, companyID, job_location, required_skills, application_deadline, salary_range, job_category)" +
+            "VALUES (@JobID, @JobTitle, @JobDescription, @CompanyID, @JobLocation, @RequiredSkills, @ApplicationDeadline, @SalaryRange, @JobCategory)", connection);
+            *//*if (jobIDText.Text.Length != 0)
+            {
+                try
+                {
+                    int job_id = Int32.Parse(jobIDText.Text);
+                    if (jobCompanyIDText.Text.Length != 0)
+                    {
+                        int company_id = Int32.Parse(jobCompanyIDText.Text);
+                        cmd.Parameters.Add("@JobID", SqlDbType.Int);
+                        cmd.Parameters["@JobID"].Value = job_id;
+
+                        cmd.Parameters.Add("@JobTitle", SqlDbType.VarChar, 50);
+                        cmd.Parameters["@JobTitle"].Value = jobTitleText.Text;
+
+                        cmd.Parameters.Add("@JobDescription", SqlDbType.VarChar, 50);
+                        cmd.Parameters["@JobDescription"].Value = jobDescText.Text;
+
+                        cmd.Parameters.Add("@CompanyID", SqlDbType.Int);
+                        cmd.Parameters["@CompanyID"].Value = company_id;
+
+                        cmd.Parameters.Add("@JobLocation", SqlDbType.VarChar, 50);
+                        cmd.Parameters["@JobLocation"].Value = jobLocationText.Text;
+
+                        cmd.Parameters.Add("@RequiredSkills", SqlDbType.VarChar, 50);
+                        cmd.Parameters["@RequiredSkills"].Value = recSkillsText.Text;
+
+                        cmd.Parameters.Add("@ApplicationDeadline", SqlDbType.Date);
+                        cmd.Parameters["@ApplicationDeadline"].Value = appDeadlineText.Text;
+
+                        cmd.Parameters.Add("@SalaryRange", SqlDbType.VarChar, 50);
+                        cmd.Parameters["@SalaryRange"].Value = salaryRangeText.Text;
+
+                        cmd.Parameters.Add("@JobCategory", SqlDbType.VarChar, 50);
+                        cmd.Parameters["@JobCategory"].Value = jobCatText.Text;
+                        daJob.InsertCommand = cmd;
+                        daJob.InsertCommand.ExecuteNonQuery();
+                        ReloadJobTableView();
+                    }
+                    else
+                    {
+                        MessageBox.Show("give a company id");
+                    }
+                }
+                catch(FormatException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("enter a job id");
+            }*//*
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("DELETE FROM Job WHERE jobID=@JobID", connection);
+            *//*if (jobIDText.Text.Length != 0)
+            {
+                int job_id = Int32.Parse(jobCompanyIDText.Text);
+                command.Parameters.Add("@JobID", SqlDbType.Int);
+                command.Parameters["@JobID"].Value = job_id;
+                daEmployer.DeleteCommand = command;
+                int noDeletedJobs = daJob.DeleteCommand.ExecuteNonQuery();
+                if (noDeletedJobs == 0)
+                {
+                    MessageBox.Show("no job with this id");
+                }
+                else
+                {
+                    ReloadJobTableView();
+                }
+            }*//*
+        }
+
+        private void JobView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void JobView_SelectionChanged(object sender, EventArgs e)
+        {
+            *//*if (JobView.SelectedRows.Count != 0)
+            {
+                DataGridViewRow selectedRow = JobView.SelectedRows[0];
+                jobIDText.Text = selectedRow.Cells[0].Value.ToString();
+                jobTitleText.Text = selectedRow.Cells[1].Value.ToString();
+                jobDescText.Text = selectedRow.Cells[2].Value.ToString();
+                recSkillsText.Text = selectedRow.Cells[3].Value.ToString();
+                appDeadlineText.Text = selectedRow.Cells[4].Value.ToString();
+                salaryRangeText.Text = selectedRow.Cells[5].Value.ToString();
+                jobCatText.Text = selectedRow.Cells[6].Value.ToString();
+            }*//*
+        }
+
+        private void EmployerView_SelectionChanged(object sender, EventArgs e)
+        {
+            if (EmployerView.SelectedRows.Count != 0)
+            {
+                DataGridViewRow selectedRow = EmployerView.SelectedRows[0];
+                daEmployer.SelectCommand = new SqlCommand("SELECT * FROM Job WHERE companyID=" + selectedRow.Cells[0].Value, connection);
+                //ReloadJobTableView();
+            }
+        }*/
+    }
+}
